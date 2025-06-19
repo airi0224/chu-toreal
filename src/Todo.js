9f7a39500dd01a8782d847bf6aa6bc9b5dc02d5d
東堤愛依璃 <higashitsutsumiaii@higashitsutsumiaiinoMacBook-Pro.local>
2025-06-19 16:23:49 +0900

    reactのチュートリアル完了

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
-  return 
+const Todo = ({todo, toggleTodo }) => {
+
+    const handleTodoClick = () => {
+        toggleTodo(todo.id);
+    };
+  return (
     <div>
         <label>
-            <input type="checkbox" checked={todo.completed}/>
+            <input 
+             type="checkbox" 
+             checked={todo.completed} 
+             readOnly 
+             onChange={handleTodoClick}
+            />
         </label>
       {todo.name}
-    </div>;
+    </div>);
   
 };
 
